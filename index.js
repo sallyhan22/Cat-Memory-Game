fda42f97147d13801b1a2595d51db208cd3fcc91
sally <shan31@student.cs.ubc.ca>
2024-05-13 13:12:14 -0700

    implemented checking for matches

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 const cards = document.querySelectorAll('.memory-card');
-
-function flipCard() {
-    this.classList.toggle('flip');
-}
+const result = document.querySelector('#result');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+function flipCard() {
+
+// to prevent any more cards to be flipped if two have already been flipped
+    if (lockBoard) {
+        return;
+    }
+
+    if (this === firstCard) {
+        return;
+    }
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    } 
+    
+    secondCard = this;
+    checkForMatch();
+
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    if (isMatch) {
+        disableFlipping();
+        // update the score
+    } else {
+        unflipCards();
+    }
+    
+}
+
+function disableFlipping() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    // destructuring assignment
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
